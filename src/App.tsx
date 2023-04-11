bace667546df49ab8249cffacfc5f7c3d30f7be0
alpha0212 <qkrcjfgml321@naver.com>
2023-04-11 23:47:58 +0900

    init: main screen setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { MainPage } from '@/pages';
+import styled from '@emotion/styled';
+import { Navbar } from './components';
 
 export const App: React.FC = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+  const ToggleOnClick = () => {
+    setToggle(!toggle);
+  };
   return (
-    <Routes>
-      <Route index element={<MainPage />} />
-    </Routes>
+    <MainScreen>
+      <MainSection>
+        <Navbar toggleOnClick={ToggleOnClick} toggleState={toggle} />
+        <Routes>
+          <Route index element={<MainPage />} />
+        </Routes>
+      </MainSection>
+    </MainScreen>
   );
 };
+
+const MainScreen = styled.main`
+  width: 425px;
+  height: 100vh;
+
+  display: flex;
+  margin: auto;
+  background-color: #f2f2f2;
+`;
+
+const MainSection = styled.section`
+  position: relative;
+  width: 90%;
+  height: 100%;
+
+  margin: auto;
+`;
