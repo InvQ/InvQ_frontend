2a0de7d1384bc240775fa6e02cd3a5984278aaf8
alpha0212 <qkrcjfgml321@naver.com>
2023-04-12 23:30:54 +0900

    design(pages): layout mainpage

--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,5 +1,34 @@
 import styled from '@emotion/styled';
 
-export const text = styled.div`
-  color: #000000;
+export const MainPageContainer = styled.section`
+  width: 100%;
+`;
+
+export const MainPageTipContainer = styled.div`
+  width: 80%;
+  height: 8vh;
+  display: flex;
+
+  margin: auto;
+`;
+export const MainPageTip = styled.h3`
+  font-size: 11px;
+  font-weight: medium;
+
+  margin: auto auto auto 0;
+  color: #888;
+  text-align: left;
+`;
+
+export const InvQContainer = styled.div`
+  width: 90%;
+  height: 70vh;
+
+  margin: auto;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
