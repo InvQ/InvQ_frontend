0713c5499e42801f16171edce618338c73220029
alpha0212 <qkrcjfgml321@naver.com>
2023-04-14 01:59:26 +0900

    design(component): Design button in addpage

---
+++ src/components/common/AddInvQ/Button/styled.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  height: 55px;
+  position: absolute;
+
+  bottom: 0;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 100%;
+
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+  background-color: #3b88ff;
+`;
+export const ButtonText = styled.h1`
+  font-size: 17px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+`;
