f8b69c83a380597ecc342afe8d7dc9f0ecf62a88
alpha0212 <qkrcjfgml321@naver.com>
2023-04-14 02:08:34 +0900

    feat(pages): User-requested invq list and management

---
+++ src/pages/Admin/index.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useId, useState } from 'react';
+import * as S from './styled';
+import { InvQ } from '@components/Main';
+import { collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from 'firebase/firestore';
+import { ItemType } from '../Main';
+import { db } from '@/firebase';
+
+export const AdminPage: React.FC = () => {
+  const [invQs, setInvQs] = useState<ItemType[]>([
+    {
+      question: '',
+      answer: '',
+      status: false,
+    },
+  ]);
+  const [keyId, setKeyId] = useState([
+    {
+      keyId: '',
+    },
+  ]);
+  const [adminLogin, setAdminLogin] = useState('');
+  const [saveAdmin, setSaveAdmin] = useState('');
+  const invQsCollectionRef = collection(db, 'request');
+  useEffect(() => {
+    const getInvQs = async () => {
+      const data = await getDocs(invQsCollectionRef);
+      setKeyId(
+        data.docs.map((doc) => ({
+          keyId: doc.id,
+        }))
+      );
+      setInvQs(
+        data.docs.map((doc) => ({
+          question: doc.data().question,
+          answer: doc.data().answer,
+          status: doc.data().status,
+        }))
+      );
+    };
+    getInvQs();
+  }, []);
+  const updateInvQ = async (i: number, status: boolean) => {
+    const invDocRef = doc(db, 'request', keyId[i].keyId);
+    const invDoc = await getDoc(invDocRef);
+    const invField = { status: !status };
+    if (invDoc.exists()) {
+      await updateDoc(invDocRef, invField);
+    } else {
+      console.log(`Document with ID ${i} does not exist.`);
+    }
+  };
+  const deleteInvQ = async (i: number) => {
+    const invDocRef = doc(db, 'request', keyId[i].keyId);
+    await deleteDoc(invDocRef);
+  };
+
+  const invQArr = invQs.map((inv, i) =>
+    inv.status ? (
+      <strong key={i}>등록 요청이 없습니다.</strong>
+    ) : (
+      <div key={i}>
+        <InvQ question={inv.question} answer={inv.answer} />
+        <S.AdminButtonContainer>
+          <S.CancelButton
+            onClick={() => {
+              deleteInvQ(i);
+            }}
+          >
+            <S.CancelText>삭제</S.CancelText>
+          </S.CancelButton>
+          <S.AgreeButton
+            onClick={() => {
+              updateInvQ(i, inv.status);
+            }}
+          >
+            <S.AgreeText>수락</S.AgreeText>
+          </S.AgreeButton>
+        </S.AdminButtonContainer>
+      </div>
+    )
+  );
+  return (
+    <>
+      {saveAdmin === import.meta.env.VITE_ADMIN_KEY ? (
+        <S.AdminContainer>
+          <S.AdminPageTipContainer>
+            <S.AdminPageTip>요청에 대해 수락 또는 거절해 보세요.</S.AdminPageTip>
+          </S.AdminPageTipContainer>
+          <S.InvRContainer>{invQArr}</S.InvRContainer>
+        </S.AdminContainer>
+      ) : (
+        <>
+          <strong>인증해주세요</strong>
+          <input type="password" onChange={(event) => setAdminLogin(event.target.value)} />
+          <button onClick={() => setSaveAdmin(adminLogin)}>인증</button>
+        </>
+      )}
+    </>
+  );
+};
