0361f74d365266b092d6301a3d4b880587ada9cc
alpha0212 <qkrcjfgml321@naver.com>
2023-04-14 02:06:48 +0900

    design(pages): Add InvQContainer in InvQPage

---
+++ src/pages/SelectPage/InvQPage/styled.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const InvQContainer = styled.div`
+  width: 90%;
+  height: 70vh;
+
+  margin: auto;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
