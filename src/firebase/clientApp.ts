bc1977c5f7c1cbf97670740e209fe570441a4d56
alpha0212 <qkrcjfgml321@naver.com>
2023-04-14 02:02:52 +0900

    feat(firebase): Initial setting

---
+++ src/firebase/clientApp.ts
@@ -0,0 +1,33 @@
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID,
+  VITE_FIREBASE_MEASUREMENT_ID,
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+//기본 firebase
+const app = initializeApp(firebaseConfig);
+const firebase = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+export const db = getFirestore(app);
+
+export { app, firebase, auth, storage };
