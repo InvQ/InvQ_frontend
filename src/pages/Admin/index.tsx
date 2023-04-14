8ec6d5fbb395dce19c3d5a0f950e63cfb70cf8a2
alpha0212 <qkrcjfgml321@naver.com>
2023-04-14 14:55:12 +0900

    refactor: Improving useCallBack performance

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useId, useState } from 'react';
+import React, { useCallback, useEffect, useId, useState } from 'react';
 import * as S from './styled';
 import { InvQ } from '@components/Main';
 import { collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from 'firebase/firestore';
@@ -20,43 +20,49 @@ export const AdminPage: React.FC = () => {
   ]);
   const [adminLogin, setAdminLogin] = useState('');
   const [saveAdmin, setSaveAdmin] = useState('');
-  const invQsCollectionRef = collection(db, 'request');
-  useEffect(() => {
-    const getInvQs = async () => {
-      const data = await getDocs(invQsCollectionRef);
-      setKeyId(
-        data.docs.map((doc) => ({
-          keyId: doc.id,
-        }))
-      );
-      setInvQs(
-        data.docs.map((doc) => ({
-          question: doc.data().question,
-          answer: doc.data().answer,
-          status: doc.data().status,
-        }))
-      );
-    };
-    getInvQs();
+  const invQsCollectionRef = collection(db, 'invqRq');
+  const getInvQs = useCallback(async () => {
+    const data = await getDocs(invQsCollectionRef);
+    const invQsData = data.docs.map((doc) => ({
+      question: doc.data().question,
+      answer: doc.data().answer,
+      status: doc.data().status,
+    }));
+    setInvQs(invQsData);
+    const keyIdData = data.docs.map((doc) => ({
+      keyId: doc.id,
+    }));
+    setKeyId(keyIdData);
   }, [invQsCollectionRef]);
-  const updateInvQ = async (i: number, status: boolean) => {
-    const invDocRef = doc(db, 'request', keyId[i].keyId);
-    const invDoc = await getDoc(invDocRef);
-    const invField = { status: !status };
-    if (invDoc.exists()) {
-      await updateDoc(invDocRef, invField);
-    } else {
-      console.log(`Document with ID ${i} does not exist.`);
-    }
-  };
-  const deleteInvQ = async (i: number) => {
-    const invDocRef = doc(db, 'request', keyId[i].keyId);
-    await deleteDoc(invDocRef);
-  };
+
+  useEffect(() => {
+    getInvQs();
+  }, [getInvQs]);
+  const updateInvQ = useCallback(
+    async (i: number, status: boolean) => {
+      const invDocRef = doc(db, 'invqRq', keyId[i].keyId);
+      const invDoc = await getDoc(invDocRef);
+      const invField = { status: !status };
+      if (invDoc.exists()) {
+        await updateDoc(invDocRef, invField);
+      } else {
+        console.log(`Document with ID ${i} does not exist.`);
+      }
+    },
+    [db, keyId]
+  );
+
+  const deleteInvQ = useCallback(
+    async (i: number) => {
+      const invDocRef = doc(db, 'invqRq', keyId[i].keyId);
+      await deleteDoc(invDocRef);
+    },
+    [db, keyId]
+  );
 
   const invQArr = invQs.map((inv, i) =>
     inv.status ? (
-      <strong key={0}>등록 요청이 없습니다.</strong>
+      <div key={i}></div>
     ) : (
       <div key={i}>
         <InvQ question={inv.question} answer={inv.answer} />
